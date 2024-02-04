572666402445cd3c520699beb3776defa6505baf
Noah do Rego <ndore051@uottawa.ca>
2024-02-04 15:31:00 -0500

    Added mp4/mov conversion to each other

--- convert.py
+++ convert.py
@@ -17,19 +17,26 @@ for file_name in os.listdir("./input"):
     if file_extension == ".mp4":
         print("1: MP4 -> GIF")
         print("2: MP4 -> MP3")
+        print("3: MP4 -> MOV")
         conversion = input("Input: ")
     elif file_extension == ".mov":
         print("\n1: MOV -> GIF")
         print("2: MOV -> MP3")
+        print("3: MOV -> MP4")
         conversion = input("\nInput: ")
     else:
         conversion = ""
-        print("\n** Unsupported file type **")
+        print("\n** Unsupported file type (" + file_extension + ") **")
 
     if conversion == "1":
         ffmpeg.input(file_path).output("output/" + file_name + ".gif").run()
     elif conversion == "2":
         ffmpeg.input(file_path).output("output/" + file_name + ".mp3").run()
+    elif conversion == "3":
+        if file_extension == ".mp4":
+            ffmpeg.input(file_path).output("output/" + file_name + ".mov").run()
+        else:
+            ffmpeg.input(file_path).output("output/" + file_name + ".mp4").run()
     elif conversion == "":
         print("\nSkipping to next file...")
     else:
