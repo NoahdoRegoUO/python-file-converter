71ade3583655527615c332f83cd1c6494f0f4e23
Noah do Rego <ndore051@uottawa.ca>
2024-02-04 14:32:27 -0500

    Reformatted print statements

--- convert.py
+++ convert.py
@@ -1,77 +1,41 @@
 import os
 import ffmpeg
 
-print(
-    """
-    *** FILE CONVERTER ***
+print("\n*** FILE CONVERTER ***")
+print("\nTo begin, add the file you wish to convert into the 'input' folder.")
+print("\nOnce you are done, press enter.")
 
-    To begin, add the file you wish to convert into the "input" folder.
-
-    Once you are done, press enter."""
-)
-
-input(
-    """
-    [Press enter to continue]"""
-)
+input("\n[Press enter to continue]")
 
 for file_name in os.listdir("./input"):
     file_path = "input/" + file_name
 
     file_name, file_extension = os.path.splitext(file_name)
 
-    print(
-        """
-        Choose a conversion by entering a number from the list below: """
-    )
+    print("\nChoose a conversion by entering a number from the list below: ")
 
     if file_extension == ".mp4":
-        conversion = input(
-            """
-            1: MP4 -> GIF
-            2: MP4 -> MP3
-
-            Input: """
-        )
+        print("1: MP4 -> GIF")
+        print("2: MP4 -> MP3")
+        conversion = input("Input: ")
     elif file_extension == ".mov":
-        conversion = input(
-            """
-            1: MOV -> GIF
-            2: MOV -> MP3
-
-            Input: """
-        )
+        print("\n1: MOV -> GIF")
+        print("2: MOV -> MP3")
+        conversion = input("\nInput: ")
     else:
         conversion = ""
-        print(
-            """
-              ** Unsupported file type **"""
-        )
+        print("\n** Unsupported file type **")
 
     if conversion == "1":
         ffmpeg.input(file_path).output("output/" + file_name + ".gif").run()
     elif conversion == "2":
         ffmpeg.input(file_path).output("output/" + file_name + ".mp3").run()
     elif conversion == "":
-        print(
-            """
-            Skipping to next file..."""
-        )
+        print("\nSkipping to next file...")
     else:
-        print(
-            """
-            ** Invalid input **
+        print("\n** Invalid input **")
+        print("\nSkipping to next file...")
 
-            Skipping to next file..."""
-        )
+    input("\n[Press enter to continue]")
 
-    input(
-        """
-        [Press enter to continue]"""
-    )
-
-print(
-    """
-    No more files to covert. Program ending.
-    """
-)
+print("\nNo more files to covert. Program ending.\n")
